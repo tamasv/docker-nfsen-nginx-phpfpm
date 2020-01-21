627f38a0b75c12dc5dc9a443b4f686fb1bd1b48d
Tamas Varga <tamas@alapzaj.com>
2020-01-21 09:15:03 +0100

    Fixed healthcheck typo

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 command=$(/nfsen/bin/nfsen status | grep "is not running" | wc -l)
 if [[ $command  == "0" ]]; then
-        echo "$command success"
+        echo "NFSen healthcheck success"
         exit 0
-else:
-        echo "$command fail"
+else
+        echo "NFSen healthcheck failed"
         exit -1
 fi
 
